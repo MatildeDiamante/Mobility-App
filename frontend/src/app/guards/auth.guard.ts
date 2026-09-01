ba05f57de230eeedb4dad29113e5b07342bc0970
Matilde <matilde.moretti95@gmail.com>
2026-09-01 19:18:49 +0200

    Create the route guards

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+// Authentication guard to protect routes that require a logged-in user
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
-@Component({
-  selector: 'app-auth-guard',
-  standalone: true,
-  template: `<main><h1>Auth Guard</h1></main>`,
-})
-export class authGuard {}
+export const authGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router); // calls Angular's router to redirect the user to the login page
+
+  if (authService.currentUser) {
+    return true; // user authenticated, and can access the route
+  }
+
+  return router.createUrlTree(['/login']); // user not authenticated, redirect to login page
+};
--- frontend/src/app/guards/role.guard.ts
+++ frontend/src/app/guards/role.guard.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+// Guard checks if the user has the required role to access a route
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { UserRole } from '../models/auth-user.model';
 
-@Component({
-  selector: 'app-role-guard',
-  standalone: true,
-  template: `<main><h1>Role Guard</h1></main>`,
-})
-export class roleGuard {}
+export const roleGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
+  const authService = inject(AuthService); // receives the authenticated user's service
+  const router = inject(Router); // receives the router to redirect the user to the login
+
+  const requiredRole = route.data['role'] as UserRole;
+
+  // checks the authenticated's role against the required role
+  if (authService.currentUser?.role === requiredRole) {
+    return true;
+  }
+
+  return router.createUrlTree(['/login']); // the role is wrong or the user doesn't exist, the page is not opened
+};
